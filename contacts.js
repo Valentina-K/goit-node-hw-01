a1d2c7e8cf4c92ea08b399572678701b2b2df0b3
Valentina-K <kvalentyna@hotmail.com>
2023-03-11 22:33:03 +0200

    first commit

---
+++ contacts.js
@@ -0,0 +1,46 @@
+const fs = require('fs/promises');
+const path = require('path');
+const { v4 } = require("uuid");
+
+ const contactsPath = path.join(__dirname,'db','contacts.json');
+
+
+// TODO: задокументировать каждую функцию
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find(item => item.id === contactId);
+  if (!contact)
+    return null;
+  return contact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const ind = contacts.findIndex(item => item.id === contactId);
+  if (ind === -1)
+    return null;
+  const [deleteContact] = contacts.splice(ind, 1);
+  await fs.writeFile(contactsPath,JSON.stringify(contacts));
+  return deleteContact;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { id: v4(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
+}
+
+module.exports = {
+    listContacts,
+    addContact,
+    removeContact,
+    getContactById,
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+const contactsApi = require('./contacts');
+const argv = require("yargs").argv;
+
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+          const contacts = await contactsApi.listContacts();
+          console.log(contacts);
+      break;
+
+    case "get":
+          const contact = await contactsApi.getContactById(id);
+          if (!contact)
+              throw new Error(`Contact with id=${id} is not found!`);
+          console.log(contact);
+      break;
+
+    case "add":
+          const newContact = await contactsApi.addContact({ name, email, phone });
+          console.log(newContact);
+      break;
+
+    case "remove":
+          const deleteContact = await contactsApi.removeContact(id);
+          if (!contact)
+              throw new Error(`Contact with id=${id} is not found!`);
+          console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
